3af5112d79292c2211d3971dce70f611c6bb341e
ZAHIRULISLAM-CSE <neloy1613@gmail.com>
2023-05-03 23:27:22 +0600

    completed the footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Nav from './components/header/Nav'
+import Footer from './components/footer/Footer'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div className=''>
         <Nav></Nav>
         <Outlet></Outlet>
+        <Footer></Footer>
     </div>
   )
 }
