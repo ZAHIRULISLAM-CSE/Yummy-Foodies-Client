f4ef287db543e40b275bf06e89119e34f21d446f
ZAHIRULISLAM-CSE <neloy1613@gmail.com>
2023-05-03 17:47:28 +0600

    completed each chef bannar and receipe page

---
+++ src/components/chef_receipes/FamousReceipeCard.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const FamousReceipeCard = (props) => {
+    const [dis,setDis]=useState(false);
+
+    const handleToast=()=>{
+        setDis(true);
+        toast("Wow so easy!");
+    }
+
+
+  console.log(props.data);
+  const { name, ingredients, cooking_method, rating } = props.data;
+  return (
+    <div className="mt-12 ">
+      <div className="flex flex-col justify-between h-[700px]   max-w-sm p-6 bg-gray-500  rounded-lg shadow ">
+        <div>
+          <h5 className="mb-2 text-3xl font-bold tracking-tight text-gray-300 dark:text-white">
+            {name}
+          </h5>
+        </div>
+        <div>
+          <p className="text-2xl text-black mb-3">Ingredients:</p>
+          <ul>
+            {ingredients.map((single, index) => (
+              <li key={index} className="list-disc text-black ml-4">
+                {single}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <h1 className="mt-3 mb-3 text-2xl text-black ">Cooking Method:</h1>
+        <p className="text-black text-sm">{cooking_method}</p>
+        <p className="text-black text-2xl mt-2">
+          Rating:<span className="text-xl"> {rating}</span>{" "}
+        </p>
+        {
+            dis ? <button
+                disabled
+            onClick={handleToast}
+              type="button"
+              className="text-white mt-4 bg-gray-800 focus:outline-none   font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800  dark:focus:ring-gray-700 dark:border-gray-700"
+            >
+              Favourite
+            </button>:
+            <button
+            onClick={handleToast}
+              type="button"
+              className="text-white mt-4 bg-gray-800 focus:outline-none   font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800  dark:focus:ring-gray-700 dark:border-gray-700"
+            >
+              Favourite
+            </button>
+        }
+        <ToastContainer></ToastContainer>
+      </div>
+    </div>
+  );
+};
+
+export default FamousReceipeCard;
--- src/components/chef_receipes/SingleChefReceipe.jsx
+++ src/components/chef_receipes/SingleChefReceipe.jsx
@@ -3,35 +3,42 @@ import { useLoaderData } from "react-router-dom";
 import { FaRecordVinyl } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa";
 import { FaServer } from "react-icons/fa";
+import FamousReceipeCard from "./FamousReceipeCard";
 
 const SingleChefReceipe = () => {
   const data = useLoaderData();
   console.log(data);
-  const {chef_img,chef_name,experience,likes,bio,number_of_receipe}=data
+  const {chef_img,chef_name,experience,likes,bio,number_of_receipe,recipes}=data
 
   return (
     <div className="w-[85%] mx-auto ">
         <h1 className="text-4xl text-center mt-8">Chef Details</h1>
       <div
-        class="flex mt-12 flex-col items-center bg-gray-500  rounded-lg shadow md:flex-row md:max-w-9xl dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+        className="flex mt-12 flex-col items-center bg-gray-500  rounded-lg shadow md:flex-row md:max-w-9xl dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
       >
         <img
-          class="object-cover w-full rounded-t-lg h-96  md:w-96 md:rounded-none md:rounded-l-lg"
+          className="object-cover w-full rounded-t-lg h-96  md:w-96 md:rounded-none md:rounded-l-lg"
           src={chef_img}
           alt=""
         />
-        <div class="flex flex-col justify-between p-4 leading-normal">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-black dark:text-white">
+        <div className="flex flex-col justify-between p-4 leading-normal">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-black dark:text-white">
             {chef_name}
           </h5>
-          <p class="mb-3  font-medium text-black dark:text-gray-400">
+          <p className="mb-3  font-medium text-black dark:text-gray-400">
             Bio:{bio}
           </p>
-          <p className="mt-12 inline-flex items-center gap-3 text-2xl"> <FaRecordVinyl></FaRecordVinyl> Experience:<span className="text-lg ">    {experience}</span></p>
-          <p className="text-2xl inline-flex mt-4 items-center gap-3"> <FaServer></FaServer> Number of Receipes:<span className="text-lg">  {number_of_receipe}</span> </p>
-          <p className="text-2xl mt-4 inline-flex items-center gap-3"> <FaHeart></FaHeart> Likes:<span className="text-lg"> {likes}</span> </p>
+          <p className="mt-12 inline-flex items-center gap-3 text-2xl"> <FaRecordVinyl></FaRecordVinyl> Experience: {experience}</p>
+          <p className="text-2xl inline-flex mt-4 items-center gap-3"> <FaServer></FaServer> Number of Receipes:  {number_of_receipe} </p>
+          <p className="text-2xl mt-4 inline-flex items-center gap-3"> <FaHeart></FaHeart>Likes: {likes} </p>
         </div>
       </div>
+            <p className="text-4xl mt-12 text-center">Famous Receipe of {chef_name}</p>
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+                {
+                    recipes.map((singleReceipe,index)=><FamousReceipeCard data={singleReceipe} key={index}></FamousReceipeCard>)
+                }
+            </div>
     </div>
   );
 };
